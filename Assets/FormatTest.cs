b46bff5b2c60c65fdc5d9d2bc23fd9abfbd70bfd
baruchInsert-tech <baruchh@insert-tech.com>
2025-08-27 10:40:20 +0300

    Add formate test script

---
+++ Assets/FormatTest.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class FormatTest : MonoBehaviour
+{
+    private bool test;
+
+    private void Start()
+    {
+        if (test == false)
+        {
+            Debug.Log("test"); }
+    }
+
+    private void Update()
+    {
+    }
+}
