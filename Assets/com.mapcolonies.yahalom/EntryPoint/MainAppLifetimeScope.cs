b1f366b2cbdcd613a20a0e7622f6cc183ba17f18
Eugene <vollcanoflash@gmail.com>
2025-08-31 16:22:09 +0300

    fix

--- Assets/com.mapcolonies.yahalom/EntryPoint/MainAppLifetimeScope.cs
+++ Assets/com.mapcolonies.yahalom/EntryPoint/MainAppLifetimeScope.cs
@@ -1,5 +1,6 @@
 using com.mapcolonies.yahalom.InitPipeline;
 using com.mapcolonies.yahalom.Preloader;
+using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 
@@ -9,10 +10,14 @@ namespace com.mapcolonies.yahalom.EntryPoint
     {
         protected override void Configure(IContainerBuilder builder)
         {
+            Debug.Log("Begin Configure Startup Registrations");
+            
             builder.Register<PreloaderViewModel>(Lifetime.Scoped);
             builder.Register<InitializationPipeline>(Lifetime.Scoped);
             
             builder.Register<AppStartUpController>(Lifetime.Singleton).As<IAsyncStartable>();
+            
+            Debug.Log("End Configure Startup Registrations");
         }
     }
 }
