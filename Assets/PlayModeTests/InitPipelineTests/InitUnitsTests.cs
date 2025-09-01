ce0b906bc98dd148d01930a9163c921b7d9ab8c4
Eugene <vollcanoflash@gmail.com>
2025-09-01 15:58:24 +0300

    add tests

--- Assets/PlayModeTests/InitPipelineTests/InitUnitsTests.cs
+++ Assets/PlayModeTests/InitPipelineTests/InitUnitsTests.cs
@@ -18,12 +18,62 @@ namespace PlayModeTests.InitPipelineTests
                 return UniTask.CompletedTask;
             };
             ActionUnit unit = new ActionUnit("TestUnit", 1.0f, InitPolicy.Fail, action);
-           
+
             await unit.RunAsync();
-            
+
             Assert.IsTrue(executed);
         }
-        
 
+        [Test]
+        public void RunAsync_NullAction_ShouldThrow()
+        {
+            ActionUnit unit = new ActionUnit("Test", 1f, InitPolicy.Fail, () => throw new ArgumentNullException());
+
+            Assert.ThrowsAsync<ArgumentNullException>(async () => { await unit.RunAsync(); });
+        }
+
+        [Test]
+        public void RunAsync_WithFailPolicy_ShouldPropagateException()
+        {
+            ActionUnit unit = new ActionUnit("Test", 1f, InitPolicy.Fail,
+                () => { throw new InvalidOperationException("boom"); });
+
+            Assert.ThrowsAsync<InvalidOperationException>(async () => await unit.RunAsync());
+        }
+
+        [Test]
+        public async Task RunAsync_WithContinuePolicy_ShouldSwallowException()
+        {
+            ActionUnit unit = new ActionUnit("Test", 1f, InitPolicy.Ignore,
+                () => { throw new InvalidOperationException("boom"); });
+
+            // Should not throw
+            await unit.RunAsync();
+        }
+
+        [Test]
+        public async Task Test_RunAsync_RetryPolicy_ShouldRetryAndSucceed()
+        {
+            // Arrange
+            int callCount = 0;
+            Func<UniTask> action = () =>
+            {
+                callCount++;
+                if (callCount == 1)
+                {
+                    throw new InvalidOperationException("First call fails");
+                }
+
+                return UniTask.CompletedTask; // second call succeeds
+            };
+
+            ActionUnit unit = new ActionUnit("RetryUnit", 1.0f, InitPolicy.Retry, action);
+
+            // Act
+            await unit.RunAsync();
+
+            // Assert
+            Assert.AreEqual(2, callCount, "Action should be executed twice (fail then retry)");
+        }
     }
 }
