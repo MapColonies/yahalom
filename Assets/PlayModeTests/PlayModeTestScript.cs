ab8243e1f25406e441106bf83bee17731f688225
baruchInsert-tech <baruchh@insert-tech.com>
2025-09-07 11:44:46 +0300

    revert test

--- Assets/PlayModeTests/PlayModeTestScript.cs
+++ Assets/PlayModeTests/PlayModeTestScript.cs
@@ -17,8 +17,7 @@ namespace PlayModeTests
                 Assert.AreEqual(expectedName, go.name);
                 Debug.Log("GameObject_IsCreatedWithCorrectName passed.");
             }
-            catch (AssertionException e
-                    )
+            catch (AssertionException e)
             {
                 Debug.LogError($"GameObject_IsCreatedWithCorrectName failed: {e.Message}");
                 throw;
