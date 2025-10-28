289ef0e0e8b685d70b9228b7b9bb973f65f652fb
baruchInsert-tech <baruchh@insert-tech.com>
2025-10-28 08:46:42 +0200

    Add serialization test

--- Assets/EditorTests/Analytics/LogObjectTests.cs
+++ Assets/EditorTests/Analytics/LogObjectTests.cs
@@ -2,6 +2,7 @@ using System;
 using com.mapcolonies.core.Services.Analytics.Enums;
 using com.mapcolonies.core.Services.Analytics.Managers;
 using com.mapcolonies.core.Services.Analytics.Model;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using UnityEngine;
 
@@ -28,6 +29,30 @@ namespace EditorTests.Analytics
             Assert.AreEqual(component, log.Component);
             Assert.AreEqual(type, log.MessageType);
             Assert.That(log.TimeStamp, Is.InRange(DateTime.UtcNow.AddMinutes(-1), DateTime.UtcNow.AddMinutes(1)));
+        }
+
+        [Test]
+        public void Serialize_ToJson_Contains_Expected_Fields()
+        {
+            AnalyticsManager.SessionId = 987654321L;
+            var severity = LogType.Log;
+            var message = "LayerUseStarted";
+            var parameters = LayerData.Create("imagery", "layer-001");
+            var component = "General";
+            var type = AnalyticsMessageTypes.LayerUseStarted;
+
+            var log = LogObject.Create(severity, message, parameters, component, type);
+
+            string json = JsonConvert.SerializeObject(log, Formatting.None);
+
+            StringAssert.Contains("\"SessionID\":987654321", json);
+            StringAssert.Contains("\"Severity\":\"Log\"", json);
+            StringAssert.Contains("\"Message\":\"LayerUseStarted\"", json);
+            StringAssert.Contains("\"Component\":\"General\"", json);
+            StringAssert.Contains($"\"MessageType\":{(int)AnalyticsMessageTypes.LayerUseStarted}", json);
+            StringAssert.Contains("\"LayerDomain\":\"imagery\"", json);
+            StringAssert.Contains("\"UniqueLayerId\":\"layer-001\"", json);
+            StringAssert.Contains("\"TimeStamp\"", json);
         }
     }
 }
