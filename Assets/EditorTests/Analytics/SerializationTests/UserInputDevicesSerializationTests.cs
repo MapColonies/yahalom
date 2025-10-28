60adf884ae2449c348ef58b63a1e2fb497a3240d
baruchInsert-tech <baruchh@insert-tech.com>
2025-10-28 11:03:10 +0200

    pr changes

---
+++ Assets/EditorTests/Analytics/SerializationTests/UserInputDevicesSerializationTests.cs
@@ -0,0 +1,20 @@
+using System.Runtime.Serialization;
+using com.mapcolonies.core.Services.Analytics.Model;
+using NUnit.Framework;
+
+namespace EditorTests.Analytics.SerializationTests
+{
+    public class UserInputDevicesSerializationTests
+    {
+        [Test]
+        public void UserInputDevices_Serializes_Expected_Fields()
+        {
+            UserInputDevicesData devs = UserInputDevicesData.Create(new[] { "Keyboard", "Mouse" });
+            SerializationInfo info = new SerializationInfo(typeof(UserInputDevicesData), new FormatterConverter());
+            devs.GetObjectData(info, new StreamingContext());
+
+            string[] stored = (string[])info.GetValue(nameof(UserInputDevicesData.InputDevices), typeof(string[]));
+            CollectionAssert.AreEqual(new[] { "Keyboard", "Mouse" }, stored);
+        }
+    }
+}
