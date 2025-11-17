5f114467761bac6f3a49c6962af78ab58b81ede7
Evgeny <evgenygi@rafael.co.il>
2025-11-17 17:51:28 +0200

    fix

--- Assets/com.mapcolonies.yahalom/DataManagement/AppSettings/AppSettingsSelectors.cs
+++ Assets/com.mapcolonies.yahalom/DataManagement/AppSettings/AppSettingsSelectors.cs
@@ -4,9 +4,9 @@ namespace com.mapcolonies.yahalom.DataManagement.AppSettings
 {
     public class AppSettingsSelectors
     {
-        public static readonly Selector<AppSettingsState, string> UserSettingsPath = (state) => state.UserSettingsPath;
-        public static readonly Selector<AppSettingsState, string> OfflineConfigurationPathSelector = (state) => state.OfflineConfigurationPath;
+        public static readonly Selector<AppSettingsState, string> UserSettingsPath = (state) => state.UserSettingsFile;
+        public static readonly Selector<AppSettingsState, string> OfflineConfigurationPathSelector = (state) => state.OfflineConfigurationFile;
         public static readonly Selector<AppSettingsState, string> RemoteConfigurationUrlSelector = (state) => state.RemoteConfigurationUrl;
-        public static readonly Selector<AppSettingsState, string> WorkspacesPathSelector = (state) => state.WorkspacesPath;
+        public static readonly Selector<AppSettingsState, string> WorkspacesPathSelector = (state) => state.WorkspacesDirectory;
     }
 }
